032dbd43a8237edfe14e67dbc325a0c438126472
hirotask <hirotaskhashi@gmail.com>
2023-02-26 23:13:11 +0900

    [add]: Fragmentを別に分けた

---
+++ app/src/main/java/me/hirotask/android/cafeapp/ui/MapsFragment.kt
@@ -0,0 +1,39 @@
+package me.hirotask.android.cafeapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import me.hirotask.android.cafeapp.R
+
+class MapsFragment: Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().setTitle(R.string.app_name)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+    }
+}
